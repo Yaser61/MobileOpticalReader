7716bb479d672ee461b138330f6af9a38de404ad
Yaser <kalkanyaser@gmail.com>
2024-05-17 17:35:20 +0300

    Add image upload to firebase storage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mobile_optical_reader/base/base_stateful_state.dart';
@@ -6,6 +10,7 @@ import 'package:mobile_optical_reader/pages/home/components/drawer_item.dart';
 import 'package:mobile_optical_reader/pages/home/components/options_button.dart';
 import 'package:provider/provider.dart';
 
+import '../../utils/utility.dart';
 import 'home_view_model.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,6 +22,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends BaseStatefulState<HomePage> {
   late HomeViewModel vm;
+
+  String imgUrl = "";
 
   @override
   void initState() {
@@ -65,12 +72,63 @@ class _HomePageState extends BaseStatefulState<HomePage> {
                   ),
                   const SizedBox(width: 20),
                   Expanded(
-                    child: OptionsButton(
-                      buttonTitle: "Seçenek2",
-                      onClick: () {
-                        debugPrint("Seçenek2 tıklandı!!");
-                      },
-                    ),
+                    child: ValueListenableBuilder(
+                        valueListenable: vm.image,
+                        builder: (_, __, ___) {
+                          return GestureDetector(
+                            onTap: () async {
+                              imagePick(ImageSource.camera);
+                              debugPrint("Seçenek2 tıklandı!!");
+
+                              //if (vm.image == null) return;
+                              String uniqueFileName = DateTime.now()
+                                  .millisecondsSinceEpoch
+                                  .toString();
+                              Reference referenceRoot =
+                                  FirebaseStorage.instance.ref();
+                              Reference referenceDirImages =
+                                  referenceRoot.child("images");
+                              Reference referenceImageToUpload =
+                                  referenceDirImages.child(uniqueFileName);
+
+                              try {
+                                await referenceImageToUpload.putFile(
+                                  File(vm.image.value!.path),
+                                );
+                                imgUrl = await referenceImageToUpload
+                                    .getDownloadURL();
+                              } catch (error) {
+                                debugPrint(error.toString());
+                              }
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFddcbdc),
+                                border: Border.all(
+                                  width: 2,
+                                  color: Colors.black,
+                                  strokeAlign: 1,
+                                ),
+                                image: vm.image.value != null
+                                    ? DecorationImage(
+                                        image: FileImage(
+                                          File(vm.image.value!.path),
+                                        ),
+                                        fit: BoxFit.cover,
+                                      )
+                                    : const DecorationImage(
+                                        // CachedNetworkImageProvider()
+                                        image: AssetImage(
+                                          "assets/images/kucuklogo.png",
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                              ),
+                              alignment: Alignment.center,
+                              child: const Text(""),
+                            ),
+                          );
+                        }),
                   ),
                 ],
               ),
@@ -170,11 +228,11 @@ class _HomePageState extends BaseStatefulState<HomePage> {
               },
             ),
           ),
-          Padding(padding: EdgeInsets.only(right: 10, top: 10),
-          child: InkWell(
-            onTap: () {
-            },
-          )),
+          Padding(
+              padding: EdgeInsets.only(right: 10, top: 10),
+              child: InkWell(
+                onTap: () {},
+              )),
           // İsteğe bağlı olarak diğer menü öğelerini buraya ekleyebilirsiniz
         ],
       ),
@@ -182,8 +240,7 @@ class _HomePageState extends BaseStatefulState<HomePage> {
   }
 
   void imagePick(ImageSource imageSource) async {
-    // image.value = await Utility().pickImageFromGallery(imageSource);
-    // veritabanı işlemlerini burda yapabilirsin
-    // debugPrint("image path ${image.value?.path ?? ""}");
+    vm.image.value = await Utility().pickImageFromGallery(imageSource);
+    debugPrint("image path ${vm.image.value?.path ?? ""}");
   }
 }
